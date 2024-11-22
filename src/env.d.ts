309170668e88d69d613387de5e95c55606fb9757
jcruzfff <95213271+jcruzfff@users.noreply.github.com>
2024-11-22 14:12:21 +0700

    fixed logs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,36 @@
 /// <reference path="../.astro/types.d.ts" />
+
+/// <reference types="astro/client" />
+
+interface Window {
+  Chart: typeof import('chart.js');
+}
+
+declare module 'chart.js' {
+  interface ChartConfiguration {
+    options?: {
+      scales?: {
+        x?: {
+          grid?: {
+            display?: boolean;
+            border?: boolean;
+          };
+        };
+        y?: {
+          grid?: {
+            display?: boolean;
+            border?: boolean;
+            color?: string;
+          };
+          ticks?: {
+            color?: string;
+            callback?: (value: any) => string;
+            font?: {
+              size?: number;
+            };
+          };
+        };
+      };
+    };
+  }
+}
